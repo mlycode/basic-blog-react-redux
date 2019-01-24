6b3dbd64a12bd786664e26a4ad4513e623e80f0d
mlycode <m.ly@live.co.uk>
2019-01-24 11:03:00 +0000

    add Browser Router and pages

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>It takes dark in order to show light. Now then, let's play. Anything you want to do you can do here. That's the way I look when I get home late; black and blue. When you do it your way you can go anywhere you choose. Go out on a limb - that's where the fruit is.</p>
+        </div>
+    )
+}
+
+export default Home;
