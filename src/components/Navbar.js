9e0875b1676550f03856c3b4fb731803471b5418
mlycode <m.ly@live.co.uk>
2019-01-24 11:08:22 +0000

    use NavLinks for navbar links instead of a tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a className="brand-logo">mlycode</a>
+                <Link to="/" className="brand-logo">mlycode</Link>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
